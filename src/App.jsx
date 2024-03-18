03ba2b311f315c447972831377c97d33ebb32d7d
amuuo-dev <anthonmuuo92@gmail.com>
2024-03-18 07:07:43 +0300

    created a custom hook useBreed to fetch breed from api and also added prettier in my code .

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import SearchParams from './SearchParams';
+import SearchParams from "./SearchParams";
 const App = () => {
- return(
-  <div>
-    <h1>Adopt Me!</h1>
-   <SearchParams />
-  </div>
- )
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 export default App;
-
