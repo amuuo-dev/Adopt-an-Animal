e06695e44df824e5034cabc4c2361e33e2428133
amuuo-dev <anthonmuuo92@gmail.com>
2024-03-25 20:12:39 +0300

    implemented the modal in the details.jsx using portals

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import ErrorBoundary from "./ErrorBoundary";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
+import Modal from "./Modal";
 
 const Details = () => {
+  const [showModal, setShowModal] = useState(false);
   const { id } = useParams();
   //["details", id] will be passed as the queryKey to fetchPet
   //like if you dont have the ideals of the id run fetchPet go and get if for me
@@ -32,8 +35,19 @@ const Details = () => {
         <h3>{pet.name}</h3>
         <h2>
           {pet.animal} - {pet.breed} - {pet.city} , {pet.state}
-          <button>Adopt {pet.name}</button>
+          <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
           <p>{pet.description}</p>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {pet.name}?</h1>
+                <div className="buttons">
+                  <button>Yes</button>
+                  <button onClick={() => setShowModal(false)}>No</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
         </h2>
       </div>
     </div>
